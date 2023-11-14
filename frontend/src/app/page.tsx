0d6cc4be06dd45a25437e1a4bad68cd9e091da89
Pedro Grigorio <pedrooh.hxh@gmail.com>
2023-11-13 23:19:06 -0300

    prepare environment

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center bg-orange-500 p-10 font-semibold">
+      videoplatform
+    </div>
+  )
+}
---
+++ frontend/tailwind.config.ts
@@ -0,0 +1,59 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        poppins: 'var(--font-poppins)',
+        inter: 'var(--font-inter)',
+      },
+    },
+
+    colors: {
+      white: '#ffffff',
+      gray: {
+        50: '#ebeaec',
+        100: '#bfbfc4',
+        200: '#a19fa8',
+        300: '#767480',
+        400: '#5b5967',
+        500: '#322f41',
+        600: '#2e2b3b',
+        700: '#24212e',
+        800: '#1c1a24',
+        900: '#15141b',
+      },
+      red: {
+        50: '#fcecef',
+        100: '#f6c4cd',
+        200: '#f2a7b4',
+        300: '#ec7f92',
+        400: '#e9667d',
+        500: '#e3405d',
+        600: '#cf3a55',
+        700: '#a12d42',
+        800: '#7d2333',
+        900: '#5f1b27',
+      },
+      orange: {
+        50: '#fff1ee',
+        100: '#ffd4c9',
+        200: '#ffc0af',
+        300: '#ffa38a',
+        400: '#ff9174',
+        500: '#ff7551',
+        600: '#e86a4a',
+        700: '#b5533a',
+        800: '#8c402d',
+        900: '#6b3122',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
